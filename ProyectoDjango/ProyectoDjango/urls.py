b2e7528b1745321a3507a38187dba2c9ca5b1299
Unioviedu <UO244594@uniovi.es>
2018-03-20 18:27:58 +0100

    Cambio nombre de proyecto

--- ProyectoDjango/ProyectoDjango/urls.py
+++ ProyectoDjango/ProyectoDjango/urls.py
@@ -1,4 +1,4 @@
-"""ClansOfWeb URL Configuration
+"""ProyectoDjango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
